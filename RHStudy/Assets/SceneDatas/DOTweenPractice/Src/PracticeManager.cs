98be10131cdb1d731977b8424634dc6a1340e98b
hopesnow <dzm512.myu@gmail.com>
2016-08-17 14:28:20 +0900

    スクリプトのアタッチ

--- RHStudy/Assets/SceneDatas/DOTweenPractice/Src/PracticeManager.cs
+++ RHStudy/Assets/SceneDatas/DOTweenPractice/Src/PracticeManager.cs
@@ -8,13 +8,27 @@ using UnityEngine;
 public class PracticeManager : MonoBehaviour
 {
 
+  public RectTransform button;
 
 
+  private bool onClick = false;
 
-
-  private void Start()
+  public void OnClick()
   {
-    
+    this.onClick = true;
+  }
+
+  private IEnumerator Start()
+  {
+    Tweener tween = this.button.DOLocalMoveY(-100f, 0.5f).SetEase(Ease.Linear);
+
+    yield return tween.WaitForCompletion();
+
+    yield return new WaitUntil(() => this.onClick);
+
+    tween = this.button.DOLocalPath(new Vector3[]{new Vector3(0f, -100f, 0f), new Vector3(0f, 100f, 0f), new Vector3(760f, 100f, 0f)}, 1.0f);
+
+    yield return 0;
   }
     
 }
