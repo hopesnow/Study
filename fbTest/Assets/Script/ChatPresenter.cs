73e461c9c8057e8c5388e9baf7ebce8325e46bfe
hopesnow <dzm512.myu@gmail.com>
2018-02-09 22:45:41 +0900

    fbTestプロジェクトの更新

--- fbTest/Assets/Script/ChatPresenter.cs
+++ fbTest/Assets/Script/ChatPresenter.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 
-public class ChatManager : MonoBehaviour
+public class ChatPresenter : MonoBehaviour
 {
     // public ScrollRect scroll;
     public Scrollbar scrollBar;
@@ -19,13 +19,54 @@ public class ChatManager : MonoBehaviour
     private const int uid = 10517;
     private const string userName = "noname";
 
-    // private List<GameObject> textList = new List<GameObject>();
     private Dictionary<string, ChatMessageModel> messageList = new Dictionary<string, ChatMessageModel>();
     private Dictionary<string, MessagePresenter> textList = new Dictionary<string, MessagePresenter>();
 
     // 初期化処理
     public void Awake()
     {
+        // 認証周りの堀
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.SignInAnonymouslyAsync().ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                // Debug.LogError("SignInAnonymouslyAsync was canceled.");
+                AddMessage("SignInAnonymouslyAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                // Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
+                AddMessage("SignInAnonymouslyAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            // Debug.LogFormat("User signed in successfully: {0} ({1})",
+            //     newUser.DisplayName, newUser.UserId);
+            AddMessage(string.Format("User signed in successfully\n name : [{0}]\n id : [{1}]", newUser.DisplayName, newUser.UserId));
+
+            FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://msgtest-6ba4f.firebaseio.com/");
+            FirebaseApp.DefaultInstance.SetEditorP12FileName("msgTest-6593476c0cfa.p12");
+            FirebaseApp.DefaultInstance.SetEditorServiceAccountEmail("msgtest-6ba4f@appspot.gserviceaccount.com");
+            FirebaseApp.DefaultInstance.SetEditorP12Password("notasecret");
+
+            // 送信ボタンの処理
+            this.sendButton.onClick.AddListener(SendMessage);
+
+            // イベントの登録
+            this.testRef = FirebaseDatabase.DefaultInstance.GetReference("test");
+            this.testRef.ChildAdded += MessageChildAdded;
+            this.testRef.ChildChanged += MessageChildChanged;
+            this.testRef.ChildRemoved += MessageChildRemoved;
+
+            ReceiveAllMessage();
+        });
+
+        // Firebase.Auth.OAuthProvider.GetCredential(
+
+        /*
         FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://msgtest-6ba4f.firebaseio.com/");
 
         // 送信ボタンの処理
@@ -38,16 +79,19 @@ public class ChatManager : MonoBehaviour
         this.testRef.ChildRemoved += MessageChildRemoved;
 
         ReceiveAllMessage();
+        */
     }
 
     // メッセージの全受取
     public void ReceiveAllMessage()
     {
-        FirebaseDatabase.DefaultInstance.GetReference("test").GetValueAsync().ContinueWith(task =>
+        Debug.Log("メッセージの全受信開始");
+        this.testRef.GetValueAsync().ContinueWith(task =>
         {
             if (task.IsFaulted)
             {
-                Debug.LogError("取得失敗");
+                // Debug.LogError("取得失敗");
+                AddMessage(task.Exception.Message);
             }
             else if (task.IsCompleted)
             {
@@ -58,6 +102,8 @@ public class ChatManager : MonoBehaviour
                     // メッセージの追加処理
                     AddMessage(data);
                 }
+
+                Debug.Log("メッセージの全受信完了");
             }
         });
     }
@@ -92,11 +138,20 @@ public class ChatManager : MonoBehaviour
 
         // データの設定
         var msg = string.Format("{0}:{1}", model.userName, model.message);
-        textObj.Init(msg, uid == model.uid, () =>
+        textObj.Init(msg, /*uid == model.uid*/false, () =>
         {
             // メッセージの削除処理
             this.testRef.Child(data.Key).RemoveValueAsync();
         });
+    }
+
+    private void AddMessage(string str)
+    {
+        var msg = string.Format("<color=#ff0000>{0}</color>", str);
+        this.originText.gameObject.SetActive(true);
+        var textObj = Instantiate(this.originText, this.originText.transform.parent);
+        this.originText.gameObject.SetActive(false);
+        textObj.Init(msg, false, null);
     }
 
     // メッセージ編集処理
@@ -136,6 +191,10 @@ public class ChatManager : MonoBehaviour
         }
     }
 
+    /**************************/
+    /* メッセージの送受信イベント */
+    /**************************/
+
     // メッセージ追加イベント
     private void MessageChildAdded(object sender, ChildChangedEventArgs args)
     {
@@ -171,6 +230,8 @@ public class ChatManager : MonoBehaviour
 
         RemoveMessage(args.Snapshot);
     }
+
+
 
     private void OnDestroy()
     {
---
+++ fbTest/Assets/Script/DatabaseManager.cs
@@ -0,0 +1,22 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class DatabaseManager
+{
+    private static DatabaseManager instance;
+    public static DatabaseManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new DatabaseManager();
+            return instance;
+        }
+    }
+
+    public void Init(string url)
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl(url);
+    }
+}
